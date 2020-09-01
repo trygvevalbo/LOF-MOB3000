ff1a5c3d1274eb5f357e2651924bc4b94407fe5d
trygvevalbo <44316276+trygvevalbo@users.noreply.github.com>
2020-09-01 18:42:42 +0200

    Add files via upload

---
+++ LOFMOB3000/app/src/main/java/com/example/lof_mob3000/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lof_mob3000
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
