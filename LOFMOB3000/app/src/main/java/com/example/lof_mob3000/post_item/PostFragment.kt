e02914ac224f64b44b09b209ed72ec561ff2fcf8
trygvevalbo <44316276+trygvevalbo@users.noreply.github.com>
2020-09-11 10:41:58 +0200

    meir endring

--- LOFMOB3000/app/src/main/java/com/example/lof_mob3000/post_item/PostFragment.kt
+++ LOFMOB3000/app/src/main/java/com/example/lof_mob3000/post_item/PostFragment.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import com.example.lof_mob3000.R
-import kotlinx.android.synthetic.main.post_found_item.view.*
+
 
 class PostFragment : Fragment(){
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-       //inflate layout
-        val binding: PostFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.post_found_item, container, false)
-        binding.post_button_found_item.setOnClickListener{
-            findNavController().navigate(Title)
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val rootView: View = inflater.inflate(R.layout.post_found_item, container, false)
+        linearLayout = rootView.findViewById<View>(R.id.constraintLayout) as LinearLayout
+        return rootView
+
     }
 }
