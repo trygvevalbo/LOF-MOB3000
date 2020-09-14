1cde450ab061670420bf1786f84de08a64e211d0
trygvevalbo <44316276+trygvevalbo@users.noreply.github.com>
2020-09-14 13:48:23 +0200

    ok

--- LOFMOB3000/app/src/main/java/com/example/lof_mob3000/post_item/PostFragment.kt
+++ LOFMOB3000/app/src/main/java/com/example/lof_mob3000/post_item/PostFragment.kt
@@ -5,18 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.lof_mob3000.R
+import com.example.lof_mob3000.databinding.PostFoundItemBinding
 
 
 class PostFragment : Fragment(){
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val rootView: View = inflater.inflate(R.layout.post_found_item, container, false)
-        linearLayout = rootView.findViewById<View>(R.id.constraintLayout) as LinearLayout
-        return rootView
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        val binding: PostFoundItemBinding = DataBindingUtil.inflate(
+            inflater, R.layout.post_found_item, container, false)
 
+
+        return binding.root
     }
 }
