3c98a8bef51bd47143c8cc6919a32a9a8b0a3de6
trygvevalbo <44316276+trygvevalbo@users.noreply.github.com>
2020-09-14 23:17:30 +0200

    mer fiks for form

--- app/src/main/java/com/example/lof_mob3000/MainActivity.kt
+++ app/src/main/java/com/example/lof_mob3000/MainActivity.kt
@@ -5,16 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.lof_mob3000.post_item.PostFoundItem
+import kotlinx.android.synthetic.main.activity_post_found_item.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       
-
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            val intent = Intent(this, PostFoundItem::class.java)
+            startActivity(intent)
+        }
     }
-
 
 }
