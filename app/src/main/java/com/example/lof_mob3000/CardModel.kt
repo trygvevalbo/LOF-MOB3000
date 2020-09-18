5803cbc6dd33ce83230c29c771c650cab8aa4caf
trygvevalbo <44316276+trygvevalbo@users.noreply.github.com>
2020-09-18 23:31:52 +0200

    sammenkobling på mappe på øverste nivå

---
+++ app/src/main/java/com/example/lof_mob3000/CardModel.kt
@@ -0,0 +1,5 @@
+package com.example.lof_mob3000
+
+class CardModel (val navn: String, val farge: String,val besk: String, val image: Int) {
+
+}
--- app/src/main/java/com/example/lof_mob3000/MainActivity.kt
+++ app/src/main/java/com/example/lof_mob3000/MainActivity.kt
@@ -1,30 +1,106 @@
 package com.example.lof_mob3000
 
+import android.content.ClipData
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
-import com.example.lof_mob3000.post_item.PostFoundItem
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lof_mob3000.post_item.PostLostItem
-import kotlinx.android.synthetic.main.activity_post_found_item.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    val arrayList = ArrayList<CardModel>()
+    val displaList = ArrayList<CardModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener{
-            val intent = Intent(this, PostLostItem::class.java)
-            startActivity(intent)
-        }
+        arrayList.add(CardModel("Item1", "blå", "Funnet ute på bakken", R.drawable.bigusbrainus))
+        arrayList.add(CardModel("Item2", "Svart", "Dette er et eksempel på lengere text en passer til Cardviewen. Loreum ipsum dolaro disico nipslandat.", R.drawable.throwup))
+        arrayList.add(CardModel("Item3", "hvit", "Funnet ute på bakken", R.drawable.cat))
+        arrayList.add(CardModel("Item4", "blå", "Funnet ute på bakken", R.drawable.donkey))
+        arrayList.add(CardModel("Item5", "ingen", "Funnet ute på bakken", R.drawable.donkey))
+        arrayList.add(CardModel("Item6", "rød", "Funnet ute på bakken", R.drawable.donkey))
+        arrayList.add(CardModel("Item6", "rød", "Funnet ute på bakken", R.drawable.donkey))
+        arrayList.add(CardModel("Item6", "rød", "Funnet ute på bakken", R.drawable.ic_launcher_background))
+        displaList.addAll(arrayList)
+
+        val myAdapter = RecyclerViewAdapter(displaList, this)
+
+        hovedListe.layoutManager = LinearLayoutManager(this)
+        hovedListe.adapter = myAdapter
 
 
-        val button1 = findViewById<Button>(R.id.button1)
-        button1.setOnClickListener{
-            val intent1 = Intent(this, PostFoundItem::class.java)
-            startActivity(intent1)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.searchmenu, menu)
+        val menuItem = menu!!.findItem(R.id.searchBar)
+        val submitPost = menu!!.findItem(R.id.submitButton)
+
+        if(submitPost != null){
+            Toast.makeText(this, "du har trykket submit", Toast.LENGTH_LONG).show()
         }
+
+        if (menuItem != null) {
+
+            val searchView = menuItem.actionView as SearchView
+            val editText = searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+            editText.hint = "Search..."
+
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+
+                    if (newText!!.isNotEmpty()) {
+                        displaList.clear()
+                        val search = newText.toLowerCase(Locale.getDefault())
+                        arrayList.forEach {
+
+                            if (it.navn.toLowerCase(Locale.getDefault()).contains(search) or (it.farge.toLowerCase(Locale.getDefault()).contains(search))){
+                                displaList.add(it)
+                            }
+                        }
+
+                        hovedListe.adapter!!.notifyDataSetChanged()
+                    }
+                    else {
+                        displaList.clear()
+                        displaList.addAll(arrayList)
+                        hovedListe.adapter!!.notifyDataSetChanged()
+                    }
+
+                    return true
+                }
+
+            })
+        }
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 
 }
