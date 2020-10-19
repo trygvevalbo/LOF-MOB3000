55eef471e4ad939f6da51d0155c7c544aaa0c9bd
trygvevalbo <44316276+trygvevalbo@users.noreply.github.com>
2020-10-19 17:13:07 +0200

    before fuckup med lesing fra database

--- app/src/main/java/com/example/noeTaptNoeFunnetAPP/FrontPage.kt
+++ app/src/main/java/com/example/noeTaptNoeFunnetAPP/FrontPage.kt
@@ -19,6 +19,8 @@ import com.example.noeTaptNoeFunnetAPP.post_item.PostLostItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
@@ -55,8 +57,10 @@ class FrontPage : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
 
+
         val date = getCurrentDateTime()
         val datenow = date.toString("dd/MM/yyyy HH:mm")
+
 
             arrayList.add(CardModel("Item1", "Funnet","blå", "Funnet ute på bakken", R.drawable.bigusbrainus, datenow))
             arrayList.add(CardModel("Item2","Funnet", "Svart", "Dette er et eksempel på lengere text en passer til Cardviewen. Loreum ipsum dolaro disico nipslandat.", R.drawable.throwup, datenow))
--- app/src/main/java/com/example/noeTaptNoeFunnetAPP/LoginActivity.kt
+++ app/src/main/java/com/example/noeTaptNoeFunnetAPP/LoginActivity.kt
@@ -3,24 +3,32 @@ package com.example.noeTaptNoeFunnetAPP
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.noeTaptNoeFunnetAPP.post_item.PostLostItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.squareup.okhttp.Dispatcher
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_signup.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginActivity: AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private var firstTimeUser = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
 
+        buttonListener()
         signup_btn.setOnClickListener{
             val intent1 = Intent(this, SignupActivity::class.java)
             startActivity(intent1)
@@ -28,9 +36,44 @@ class LoginActivity: AppCompatActivity() {
 
     }
 
+    private fun buttonListener(){
+        login_btn.setOnClickListener {
+       //     loginUser()
+        }
+
+        signup_btn.setOnClickListener{
+            val intent1 = Intent(this, SignupActivity::class.java)
+            startActivity(intent1)
+        }
+    }
+
+   /* private fun loginUser(){
+        val email = login_email.text.toString()
+        val password = login_password.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            GlobalScope.launch(Dispatchers.IO){
+                try{
+                    auth.signInWithEmailAndPassword(email,password).await()
+
+               withContext(Dispatchers.Main){
+                   Toast.makeText(this@LoginActivity,"Du er nå logget inn", Toast.LENGTH_SHORT).show()
+
+                   val i = Intent(this@LoginActivity, ???????::class.java)
+                   startActivity(i)
+                   finish()
+               }
+                }catch (e: Exception){
+                    Toast.makeText(this@LoginActivity, e.message.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }*/
+
     public override fun onStart() {
         auth = Firebase.auth
         super.onStart()
+       // checkIfUserIsLoggedIn()
         //Is User signed in check
         val currentUser = auth.currentUser
         updateUI(currentUser)
@@ -39,4 +82,11 @@ class LoginActivity: AppCompatActivity() {
     fun updateUI(currentUser : FirebaseUser?) {
 
     }
+    /*private fun checkIfUserIsLoggedIn(){
+        if(auth.currentUser != null){
+            val i = Intent(this@LoginActivity, ???????::class.java)
+            startActivity(i)
+            finish()
+        }
+    }*/
 }
